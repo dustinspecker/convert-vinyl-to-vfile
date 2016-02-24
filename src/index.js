8fde5bbc78049bf4b762b7b5935848bf6c75692d
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-02-23 18:06:27 -0600

    chore(deps): update eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-'use strict';
-import path from 'path';
-import VFile from 'vfile';
-import Vinyl from 'vinyl';
+'use strict'
+import path from 'path'
+import VFile from 'vfile'
+import Vinyl from 'vinyl'
 
 /**
  * Convert a Vinyl file to a VFile
@@ -10,28 +10,28 @@ import Vinyl from 'vinyl';
  * @return {VFile} - VFile version of vinyl
  */
 module.exports = function (vinyl) {
-  let contents, directory, extension, filename, newVinyl;
+  let newVinyl
 
   // When a "Vinyl file" is passed from a Gulp stream
   // Vinyl.isVinyl(vinyl) returns false.
   // This forces a potential Vinyl file to be a Vinyl file.
   if (vinyl) {
-    newVinyl = new Vinyl(vinyl);
+    newVinyl = new Vinyl(vinyl)
   }
 
   if (!Vinyl.isVinyl(newVinyl)) {
-    throw new TypeError('Expected a Vinyl file');
+    throw new TypeError('Expected a Vinyl file')
   }
 
-  contents = newVinyl.contents.toString();
-  directory = path.dirname(newVinyl.path);
-  extension = path.extname(newVinyl.path).replace('.', '');
-  filename = path.basename(newVinyl.path, `.${extension}`);
+  const contents = newVinyl.contents.toString()
+  const directory = path.dirname(newVinyl.path)
+  const extension = path.extname(newVinyl.path).replace('.', '')
+  const filename = path.basename(newVinyl.path, `.${extension}`)
 
   return new VFile({
     directory,
     filename,
     extension,
     contents
-  });
-};
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,54 +1,52 @@
 /* global describe, beforeEach, it */
-'use strict';
-import convertVinylToVfile from '../lib/';
-import {expect} from 'chai';
-import {join} from 'path';
-import Vinyl from 'vinyl';
+'use strict'
+import convertVinylToVfile from '../lib/'
+import {expect} from 'chai'
+import {join} from 'path'
+import Vinyl from 'vinyl'
 
 describe('conver-vinyl-to-vfile', () => {
-  let contents, directory, extension, filename, vinylFile;
+  let directory, extension, filename, vinylFile
 
   beforeEach(() => {
-    contents = 'abe lincoln';
-    directory = join('users', 'dustin', 'project');
-    extension = 'md';
-    filename = 'awesome.project';
+    const contents = 'abe lincoln'
+    directory = join('users', 'dustin', 'project')
+    extension = 'md'
+    filename = 'awesome.project'
 
     vinylFile = new Vinyl({
       contents: new Buffer(contents),
       path: join(directory, `${filename}.${extension}`)
-    });
-  });
+    })
+  })
 
   it('should throw error if vinyl if not a Vinyl file', () => {
-    function test() {
-      convertVinylToVfile();
-    }
+    const test = () => convertVinylToVfile()
 
-    expect(test).to.throw(TypeError, /Expected a Vinyl file/);
-  });
+    expect(test).to.throw(TypeError, /Expected a Vinyl file/)
+  })
 
   describe('output', () => {
-    let result;
+    let result
 
     beforeEach(() => {
-      result = convertVinylToVfile(vinylFile);
-    });
+      result = convertVinylToVfile(vinylFile)
+    })
 
     it('should have a directory property', () => {
-      expect(result.directory).to.eql(directory);
-    });
+      expect(result.directory).to.eql(directory)
+    })
 
     it('should have a filename property', () => {
-      expect(result.filename).to.eql(filename);
-    });
+      expect(result.filename).to.eql(filename)
+    })
 
     it('should have an extension property', () => {
-      expect(result.extension).to.eql(extension);
-    });
+      expect(result.extension).to.eql(extension)
+    })
 
     it('should have a contents property', () => {
-      expect(result.contents).to.eql('abe lincoln');
-    });
-  });
-});
+      expect(result.contents).to.eql('abe lincoln')
+    })
+  })
+})
